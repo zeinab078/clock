209edbb45d9bba24045c28db0e99441958db0802
zeinab Ariyan Pour <zeinabariyanpour1999@gmail.com>
2022-09-17 06:01:08 -0700

    write showTime function and use Date  and setInterval

--- clock.js
+++ clock.js
@@ -4,4 +4,18 @@ function showTime(){
     let m=date.getMinutes();
     let s=date.getSeconds();
     let status="AM";
+    if(h=== 0){
+        h=12;
+    }
+    if(h > 12){
+        h=h-12;
+        status="PM"
+    }
+    h=(h<10)?`0${h}`: h;
+    m=(m<10)? `0${m}` : m;
+    s=(s<10)? `0${s}` : s;
+     
+     let time=`${h} : ${m} : ${s}  ${status}`
+     document.querySelector(".clock").innerText=time;
 }
+setInterval(showTime, 1000)
